f71372110f9f6c484ddea11826ce9f5670b01d90
EduardoCR <educr90@gmail.com>
2021-11-01 20:18:24 +0000

    proyecto finalizado

--- PruebaAngularFrontend/src/app/model/Country.ts
+++
@@ -1,9 +0,0 @@
-export interface Country{
-    'id' : number;
-    'name' : string;
-    'capital' : string;
-    /*'gps' : {
-        lat : number,
-        long : number
-    };*/
-}
--- PruebaAngularFrontend/src/app/services/data.service.ts
+++ PruebaAngularFrontend/src/app/services/data.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Country } from '../model/Country'
 import { environment } from '../../environments/environment'
 
 @Injectable({
